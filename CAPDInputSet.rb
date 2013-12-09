e5b7cf132e3d043705cac01e54cb495cf0a80329
Jim Flanagan <flanagan.james@gmail.com>
2013-12-09 13:02:46 -0700

    frame ok

--- CAPDInputSet.rb
+++ CAPDInputSet.rb
@@ -1,59 +1,40 @@
 require 'tk'
 
 root = TkRoot.new() {
-    title "File IO"
-    width = 1000
-    height = 1000
-    
+    title "CAPD Input Set"
 }
 #filenameset = TkVariable.new([])
 $filenameset = []
 $currentfileindex
 files = {}
 
-n = Tk::Tile::Notebook.new(root)do
-    height 110
-    place('height' => 1000, 'width' => 1000, 'x' => 10, 'y' => 10)
+frame1 = TkFrame.new(root) {
+    width = 500
+    height = 1000
+    pack('side'=>'left' )
+}
+
+frame2 = TkFrame.new(root) {
+    width = 2500
+    height = 1000
+    pack('side'=>'right' )
+}
+
+notebook = Tk::Tile::Notebook.new(frame2)do
+    #height 110
+    place('height' => 1000, 'width' => 2500, 'x' => 10, 'y' => 10)
     pack()
 end
 
-f1 = TkFrame.new(n)
-f2 = TkFrame.new(n)
-f3 = TkFrame.new(n)
+f1 = TkFrame.new(notebook)
+f2 = TkFrame.new(notebook)
 
-n.add f1, :text => 'Select'
-n.add f2, :text => 'Text'
-n.add f3, :text => 'Detail'
+notebook.add f1, :text => 'Text'
+notebook.add f2, :text => 'Detail'
 
 
-require 'tk'
-
-root = TkRoot.new() {
-    title "File IO"
-    width = 1000
-    height = 500
-}
-#filenameset = TkVariable.new([])
-$filenameset = []
-$currentfileindex
-files = {}
-
-n = Tk::Tile::Notebook.new(root)do
-    height 110
-    place('height' => 500, 'width' => 1000, 'x' => 10, 'y' => 10)
-end
-
-f1 = TkFrame.new(n)
-f2 = TkFrame.new(n)
-f3 = TkFrame.new(n)
-
-n.add f1, :text => 'Select'
-n.add f2, :text => 'Text'
-n.add f3, :text => 'Detail'
-
-
-button = TkButton.new(f1) {
-    text "Browse..."
+button = TkButton.new(frame1) {
+    text "Select Files..."
     command {filestring = Tk::getOpenFile({"multiple" => true})
         filepathset = filestring.split(' ')
         $filepath = "/" + filepathset[0].scan(/\/(.+\/)+/)[0][0]
@@ -65,26 +46,27 @@ button = TkButton.new(f1) {
             # puts "afile after gsub #{afile}"
             $filenameset.push(afile)
         }
-        list = TkListbox.new(f1) do
+        list = TkListbox.new(frame1) do
             width = 50
             height = 20
             selectmode = 'single'
             bind("<ListboxSelect>") do setcurrentfile end
             #listvariable = filenameset
-            pack('side'=>'left', 'fill'=>'both', 'expand'=>true)
+            pack('side'=>'bottom', 'fill'=>'both', 'expand'=>true)
         end
         $filenameset.each {|afile| list.insert('end',afile)}
         #list.selection(list.get(0))
         #currentfile = list.curselection[0]
     }
 }
-button.pack()
+button.pack('side'=>'top')
 
-$currentfile = TkEntry.new(f1){ text "Selected File" }
-$currentfile.pack()
+$currentfile = TkEntry.new(frame1){ text "Selected File" }
+$currentfile.pack('side' => 'top')
 
 
-$currentfiletext = TkText.new(f2){
+
+$currentfiletext = TkText.new(f1){
     width = 250
     height = 20
     pack('side'=>'right', 'fill'=>'both', 'expand'=>true)
@@ -99,14 +81,14 @@ def setcurrentfile
     afile = File.open(afilename, "r")
     $currentfilerows = File.readlines(afilename).map { |line| line.chomp }
     #$currentfilerows = File.readlines(afilename)
-    puts $currentfilerows
+    #puts $currentfilerows
     $\ = "\n"
     $currentfiletextstring = $currentfilerows.join($\)
     #$currentfiletextstring = $currentfilerows.join()
     $currentfiletext.value = $currentfiletextstring
-    testfilename = $filepath + "testfile.txt"
-    testfile = File.open(testfilename,'w')
-    testfile.syswrite($currentfiletextstring)
+    #testfilename = $filepath + "testfile.txt"
+    #testfile = File.open(testfilename,'w')
+    #testfile.syswrite($currentfiletextstring)
 end
 
 
